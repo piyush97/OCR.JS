8721779e02e9de07ca3163eb7abe8052b80d16db
Piyush Mehta <me@piyushmehta.com>
2019-09-24 15:35:03 +0530

    callback for storage

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const app = express();
 const fs = require("fs");
 const multer = require("multer");
 const { TesseractWorker } = require("tesseract.js");
+const worker = new TesseractWorker();
+
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "./uploads");
+  }
+});
